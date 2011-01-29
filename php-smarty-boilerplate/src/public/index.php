1d7ff5f841feb3a330a38b70303754140d8d1821
Markus Kaiserswerth <mkai@sensun.org>
2011-01-30 00:58:30 +0100

    Added default variables for controller and action names

--- php-smarty-boilerplate/src/public/index.php
+++ php-smarty-boilerplate/src/public/index.php
@@ -1,9 +1,13 @@
 <?php
 require_once('../share/setup.php');
 
+// set defaults (used when no param is given, see below)
+$default_controller = 'default';
+$default_action = 'index';
+
 // get controller name and action from GET params
-$controller = (isset($_GET['controller']) && trim($_GET['controller'] != '') ? $_GET['controller'] : 'default');
-$action = (isset($_GET['action']) && trim($_GET['action'] != '') ? $_GET['action'] : 'index');
+$controller = (isset($_GET['controller']) && trim($_GET['controller'] != '') ? $_GET['controller'] : $default_controller);
+$action = (isset($_GET['action']) && trim($_GET['action'] != '') ? $_GET['action'] : $default_action);
 
 $template->assign('page_name', $action);
 
