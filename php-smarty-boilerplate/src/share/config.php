1564d86e01747bf4180eeaf6d146f39f7f2a978c
Markus Kaiserswerth <mkai@sensun.org>
2011-02-13 16:32:50 +0100

    Rename assets to static

--- php-smarty-boilerplate/src/share/config.php
+++ php-smarty-boilerplate/src/share/config.php
@@ -7,20 +7,20 @@ $config['deployment_hostnames'] = array('example.com', 'www.example.com');
 $config['debug'] = !(in_array($_SERVER['SERVER_NAME'], $config['deployment_hostnames']));
 
 // ...
-$config['app_name'] = 'my-page';
+$config['app_name'] = 'mypage';
 $config['app_title'] = 'My page';
 
 // server-related
 $config['document_root'] = dirname(__FILE__).'/../public/';
 $config['request_url'] = implode('/', array_slice(explode('/', 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']), 0, -1)).'/';
-$config['media_url'] = str_replace('http:', '', $config['request_url'].'assets/');
+$config['media_url'] = str_replace('http:', '', $config['request_url'].'static/');
 
 // session support
 $config['sessions_enabled'] = false;
 $config['session_save_path'] = session_save_path();
 
 // log file
-$config['logfile'] = $config['document_root'].'/../../logs/'.$config['app_name'].($config['debug'] ? '_debug' : '').'.log';
+$config['logfile'] = $config['document_root'].'../../log/'.$config['app_name'].($config['debug'] ? '_debug' : '').'.log';
 
 // Smarty template engine
 $config['smarty'] = array();
