91b79a943d658d44a4a1e362dff8e21575bf6ecb
Markus Kaiserswerth <mkai@sensun.org>
2011-02-13 16:32:14 +0100

    New paths to static file dirs

--- php-smarty-boilerplate/src/share/stylesheets/config.rb
+++ php-smarty-boilerplate/src/share/stylesheets/config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-css_dir = "../../public/assets/stylesheets/"
+css_dir = "../../public/static/stylesheets/"
 sass_dir = "."
-images_dir = "../../public/assets/images/"
-javascripts_dir = "../../public/assets/scripts/"
+images_dir = "../../public/static/images/"
+javascripts_dir = "../../public/static/scripts/"
 cache_dir = "/tmp/sass-cache/"
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
