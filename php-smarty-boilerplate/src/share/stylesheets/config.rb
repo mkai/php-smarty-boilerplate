6b2f689d335ff872d5f2fdf230f63cddcfe6455a
Markus Kaiserswerth <mkai@sensun.org>
2011-02-13 14:29:43 +0100

    Now using Compass/ SASS CSS

---
+++ php-smarty-boilerplate/src/share/stylesheets/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+css_dir = "../../public/assets/stylesheets/"
+sass_dir = "."
+images_dir = "../../public/assets/images/"
+javascripts_dir = "../../public/assets/scripts/"
+cache_dir = "/tmp/sass-cache/"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# "compress"
+style = :compressed
+output_style = :compressed
